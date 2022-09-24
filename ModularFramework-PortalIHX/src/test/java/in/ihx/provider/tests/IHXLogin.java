c44b2c942856a9128ff30e7919f87844812178bf
Jitendra <jitu.achar100@gmail.com>
2022-09-24 15:27:46 +0530

    Excel commit

--- ModularFramework-PortalIHX/src/test/java/in/ihx/provider/tests/IHXLogin.java
+++ ModularFramework-PortalIHX/src/test/java/in/ihx/provider/tests/IHXLogin.java
@@ -5,16 +5,18 @@ import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.Status;
 
+import utils.TestDataProvider;
+
 
 public class IHXLogin  extends BaseTest{
 	
-	@Test
-	public void verifyUserLoginWithCorrectCredential() throws Exception {
+	@Test(dataProvider = "getUserData",dataProviderClass =TestDataProvider.class )
+	public void verifyUserLoginWithCorrectCredential(String uname,String pwd) throws Exception {
 		
-		extentTest = extent.createTest("TC-001 - verify user login with correct credentials");		
-		String uname=configProperties.getProperty("username");
+		extentTest = extent.createTest("TC-001 - verify user login with correct credentials"+uname+ "and" +pwd);		
+		
 		extentTest.log(Status.INFO, "user name is "+uname);
-		String pwd=configProperties.getProperty("password");
+		
 		extentTest.log(Status.INFO, "user password  is "+pwd);
 		loginPage.userLogin(uname, pwd);
 		extentTest.log(Status.INFO, "user logged in successfully ");
